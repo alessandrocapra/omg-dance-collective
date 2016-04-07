41046f9f555f8bb171235611668f5245041c26bb
Daniele Pastori <fullbl@mail.com>
2016-04-07 23:45:18 +0200

    ordine gif

--- server/gif.sh
+++ server/gif.sh
@@ -8,5 +8,5 @@ ffmpeg -v warning -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -
 #rm $1.png
 
 convert -delay 1 -loop 0 -layers optimize -colors 64 -fuzz 10% -ordered-dither o8x8,256 $2 $2
-gifsicle -O2 $2 $2
+gifsicle -O2 $2 > $2
 #convert -delay 1 $1  -layers OptimizeTransparency -map netscape.gif $2
