f980d40e218c03a61b265169b9472905da7943f4
Daniele Pastori <fullbl@mail.com>
2016-04-11 22:39:04 +0200

    ws

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ var Omg = function(){
 	this.cameraPreview = document.getElementById('camera');
 	this.gifContainer = document.getElementById('grid');
 	this.background = document.getElementById('background');
-	//this.serverUrl = 'https://www.omgdancecollective.gq';
-	//this.wsUrl = 'wss://www.omgdancecollective.gq/ws';
-	this.serverUrl = 'http://localhost:8080';
-	this.wsUrl = 'ws://localhost:8080/ws';
+	this.serverUrl = 'https://www.omgdancecollective.gq';
+	this.wsUrl = 'wss://www.omgdancecollective.gq/ws';
+	//this.serverUrl = 'http://localhost:8080';
+	//this.wsUrl = 'ws://localhost:8080/ws';
 	this.dimensions =  { width: 340, height: 240 }
 	this.wait = false;
 	this.timeouts = {};
@@ -232,7 +232,6 @@ Omg.prototype.startWSClient = function(){
 		return false;
 	connection = new WebSocket( this.wsUrl, 'gif' );
 	connection.onmessage = function(e){
-		console.log( 'ricevuto ws');
 		var lastRow = rec.gifContainer.lastChild, container;
 		if( lastRow.children.length >= 4 ){
 			container = document.createElement('div');
