1c9c5dd3eaf7b90c7bddbd261f2fc87e58af2bc6
Daniele Pastori <fullbl@mail.com>
2016-04-11 22:13:46 +0200

    tolto delay

--- server/gif.sh
+++ server/gif.sh
@@ -6,6 +6,6 @@ ffmpeg -v warning -i $1 -vf "$filters,palettegen" -y $palette
 ffmpeg -v warning -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $2
 rm $1.png
 
-convert -delay 2 -loop 0 -layers Optimize -colors 32 -fuzz 30% -ordered-dither o8x8,4,4,4 $2 $2
+convert -delay 1 -loop 0 -layers Optimize -colors 32 -fuzz 30% -ordered-dither o8x8,4,4,4 $2 $2
 gifsicle -O2 -o $2 $2
 #convert -delay 1 $1  -layers OptimizeTransparency -map netscape.gif $2
